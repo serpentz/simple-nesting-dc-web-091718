16c7c72fa3aeeef400b6d37176a57544557a21a6
Absalom Cherinet <absalomcherinet@gmail.com>
2018-09-05 15:24:37 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:languages]
+programmer_hash[:dennis_ritchie][:languages].to_s
 end
 
 def adding_matz
@@ -82,7 +82,7 @@ def adding_matz
         }
      }
 
-
+programmer_hash[:yukihiro_matsumoto] = {}
 end
 
 def changing_alan
